fcc5b5cd545d8c2b29ff5460e44639821b5cf9dc
xeusteerapat <xeus085@gmail.com>
2024-02-04 21:13:03 +0700

    🎉 [init] init project

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<MantineProvider>
+			<App />
+		</MantineProvider>
+	</React.StrictMode>,
+);
